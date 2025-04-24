535d4729d4a1cd302610c572c59252f21df672de
hrithik7 <hrithikjasrapuria007@gmail.com>
2025-04-24 17:26:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('active');
+    const navbar = document.querySelector('.navbar');
+    navbar.classlist.toggle('active');
+ }
+    
